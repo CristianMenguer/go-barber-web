dfba2602a23de60cf46750f7daa5ccc31236ce45
Cristian Menguer <cristianmenguer@gmail.com>
2020-07-15 14:44:18 +0100

    unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi'
+import { Form } from '@unform/web'
 
 import logoImg from '../../assets/logo.svg'
 
@@ -7,32 +8,45 @@ import { Container, Content, Background } from './styles'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
 
-const SignUp: React.FC = () => (
-    <>
-        <Container>
-            <Background />
-            <Content>
-                <img src={logoImg} alt="Go Barber" />
+interface PropsSubmit {
+    name: string
+    email: string
+    password: string
+}
 
-                <form >
-                    <h1>Sign Up</h1>
-                    <Input name="Name" placeholder="Name" icon={FiUser} />
-                    <Input name="Email" placeholder="Email" icon={FiMail} />
-                    <Input
-                        name="Password"
-                        type="password"
-                        placeholder="Password"
-                        icon={FiLock} />
-                    <Button >Sign Up</Button>
-                </form>
-                <a href="login">
-                    <FiArrowLeft />
+const SignUp: React.FC = () => {
+
+    function handleSubmit({ name, email, password }: PropsSubmit): void {
+        console.log(name, email, password)
+    }
+
+    return (
+        <>
+            <Container>
+                <Background />
+                <Content>
+                    <img src={logoImg} alt="Go Barber" />
+
+                    <Form initialData={{ name: '' }} onSubmit={handleSubmit} >
+                        <h1>Sign Up</h1>
+                        <Input name="name" placeholder="Name" icon={FiUser} />
+                        <Input name="email" placeholder="Email" icon={FiMail} />
+                        <Input
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            icon={FiLock} />
+                        <Button type="submit" >Sign Up</Button>
+                    </Form>
+                    <a href="login">
+                        <FiArrowLeft />
                     Go back to Log On
-                </a>
-            </Content>
-        </Container>
+                    </a>
+                </Content>
+            </Container>
 
-    </>
-)
+        </>
+    )
+}
 
 export default SignUp

202c706ba5e207ecd72038c9990f8d1ecef93815
Cristian Menguer <cristianmenguer@gmail.com>
2020-07-28 14:50:30 +0100

    toast-component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn'
 
 import GlobalStyle from './styles/global'
 
+import ToastContainer from './components/ToastContainer'
 import { AuthProvider } from './hooks/AuthContext'
 
 
@@ -14,6 +15,8 @@ const App: React.FC = () => (
         <AuthProvider >
             <SignIn />
         </AuthProvider>
+
+        <ToastContainer />
 
         <GlobalStyle />
     </>
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -46,8 +46,10 @@ const SignIn: React.FC = () => {
             })
 
         } catch (err) {
-            const errors = getValidationErrors(err)
-            formRef.current?.setErrors(errors)
+            if (err instanceof Yup.ValidationError) {
+                const errors = getValidationErrors(err)
+                formRef.current?.setErrors(errors)
+            }
         }
     }, [signIn])
 

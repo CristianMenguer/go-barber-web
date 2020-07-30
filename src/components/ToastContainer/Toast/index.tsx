be185df750c63ad64f73d3c5e3ec9639a6aaf07c
Cristian Menguer <cristianmenguer@gmail.com>
2020-07-30 20:02:49 +0100

    react-spring

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -1,21 +1,27 @@
 import React, { useCallback, useEffect } from 'react'
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi'
+import { FiAlertCircle, FiXCircle, FiCheckCircle, FiInfo } from 'react-icons/fi'
 
 import { ToastMessage, useToast } from '../../../hooks/toast'
 import { Container } from './styles'
 
 interface ToastProps {
     message: ToastMessage
+    style: object
 }
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const icons = {
+    info: <FiInfo size={24} />,
+    error: <FiAlertCircle size={24} />,
+    success: <FiCheckCircle size={24} />
+}
+
+const Toast: React.FC<ToastProps> = ({ message,  style}) => {
 
     const { removeToast } = useToast()
 
     useEffect(() => {
         const timer = setTimeout(() => {
             removeToast(message.id)
-            console.log('excluir')
         }, 3000)
 
         return () => {
@@ -32,8 +38,9 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
             <Container
                 hasDescription={!!message.description}
                 type={message.type}
+                style={style}
             >
-                <FiAlertCircle size={20} />
+                {icons[message.type || 'info']}
                 <div>
                     <strong>{message.title}</strong>
                     {message.description && <p>{message.description}</p> }
--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useTransition } from 'react-spring'
 
 import { ToastMessage } from '../../hooks/toast'
 import { Container } from './styles'
@@ -11,13 +12,33 @@ interface ToastContainerProps {
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
 
+    const messagesWithTransitions = useTransition(
+        messages,
+        message => message.id,
+        {
+            from: {
+                right: '-120%',
+                opacity: 0.2
+            },
+            enter: {
+                right: '0%',
+                opacity: 1
+            },
+            leave: {
+                right: '-130%',
+                opacity: 0
+            }
+        }
+    )
+
     return (
         <>
             <Container >
-                {messages.map(message => (
+                {messagesWithTransitions.map(({ item, key, props }) => (
                     <Toast
-                        key={message.id}
-                        message={message}
+                        key={key}
+                        message={item}
+                        style={props}
                     />
                 ))}
 
